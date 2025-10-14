ed983de13cbcfb0d3e87ec0dc393c0e6ec5ae761
Miksa <malaxa2000@gmail.com>
2025-10-15 00:51:17 +0300

    Initial Commit

---
+++ Musica/MusicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicaApp.swift
+//  Musica
+//
+//  Created by Miksa on 15.10.25.
+//
+
+import SwiftUI
+
+@main
+struct MusicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
